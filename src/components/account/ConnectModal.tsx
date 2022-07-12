b19fd17b7275f4e8fbf33d1c00259420c5d60796
JacquesPoulin <jacques.poulin64@gmail.com>
2022-07-12 18:43:37 +0200

    [MERGING FIX]

--- src/components/account/ConnectModal.tsx
+++ src/components/account/ConnectModal.tsx
@@ -8,20 +8,31 @@ import FormControl from '@mui/material/FormControl';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import React, { useState } from 'react';
+import axios from 'axios';
+import IUser from '../../interfaces/IUser';
 import { Link } from 'react-router-dom';
-import { GoogleLoginButton } from 'react-social-login-buttons';
-import { FacebookLoginButton } from 'react-social-login-buttons';
+// import { GoogleLoginButton } from 'react-social-login-buttons';
+// import { FacebookLoginButton } from 'react-social-login-buttons';
 
 // --------------------------------------------------------------
 
 const ConnectModal = () => {
   // >> STATES
 
+  // ---- for the firstName ----
+  const [firstName, setFirstName] = useState<string>('');
+
+  // ---- for the lastName ----
+  const [lastName, setLastName] = useState<string>('');
+
   // ---- for the email ----
   const [email, setEmail] = useState<string>('');
 
   // ---- for the password ----
   const [password, setPassword] = useState<string>('');
+
+  // ---- for the phoneNumber ----
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
 
   // ---- for the confirmed password ----
   const [confirmedPassword, setConfirmedPassword] = useState<string>('');
@@ -41,14 +52,29 @@ const ConnectModal = () => {
   // ---- for the checkbox ----
   const [isChecked, setIsChecked] = useState<boolean>(false);
 
-  // >> MY FUNCTIONS
+  // ---- for the checkbox ----
+  const [errorMessage, setErrorMessage] = useState<string>('');
 
-  // ---- to handle click on the socials buttons ------
-  const displayHello = () => alert('Social Authentication OK !');
+  // >> MY FUNCTIONS
 
   // ---- to handle checkbox click ------
   const handleChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(e.target.checked);
+  };
+
+  // ---- to update firstName change  ------
+  const handleFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFirstName(e.target.value);
+  };
+
+  // ---- to update lastName change  ------
+  const handleLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLastName(e.target.value);
+  };
+
+  // ---- to update PhoneNumber change  ------
+  const handlePhoneNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
   };
 
   // ---- to update Email change  ------
@@ -83,6 +109,28 @@ const ConnectModal = () => {
     setCity(e.target.value);
   };
 
+  // const validatePhoneNumber = (phone: string) => {
+  //   const pattern = /^\(?(\d{2})\)?[- ]?(\d{2})[- ]?(\d{2})[- ]?(\d{2})[- ]?(\d{2})$/;
+  //   return pattern.test(phone);
+  // };
+
+  // ---- to handle the form submit  ------
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setEmail('');
+    setFirstName('');
+    setLastName('');
+    setPhoneNumber('');
+    setPassword('');
+    setAddress1('');
+    setAddress2('');
+    setPassword('');
+    setConfirmedPassword('');
+    setZipCode('');
+    setCity('');
+    setIsChecked(false);
+  };
+
   // ---- to check if password === confirmed password  ------
   // const checkPassword = (e: React.ChangeEvent<HTMLInputElement>) => {
   //   return password !== confirmedPassword ? true : false;
@@ -91,32 +139,118 @@ const ConnectModal = () => {
   // >> MY VARIABLES
 
   // ------ store the social icons styles ------
-  const socialIconAlign: string | any = 'center';
-  const facebookText: string = 'Se connecter avec Facebook';
-  const googleText: string = 'Se connecter avec Google';
+  // const socialIconAlign: string | any = 'center';
+  // const facebookText: string = 'Se connecter avec Facebook';
+  // const googleText: string = 'Se connecter avec Google';
 
   // ------ Pattern for the email input ------
-  const emailPattern = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/gi;
+  // const emailPattern = new RegExp('A[A-Z0-9+_.-]+@[A-Z0-9.-]+Z');
+
+  // >> AXIOS
+
+  const postUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      await axios.post<IUser>(`${import.meta.env.VITE_DB_URL}api/users`, {
+        firstName,
+        lastName,
+        email,
+        password,
+        address1,
+      });
+      setEmail('');
+      setFirstName('');
+      setLastName('');
+      setPhoneNumber('');
+      setPassword('');
+      setAddress1('');
+      setAddress2('');
+      setPassword('');
+      setConfirmedPassword('');
+      setZipCode('');
+      setCity('');
+      setIsChecked(false);
+    } catch (err) {
+      if (axios.isAxiosError(err)) {
+        // pour gérer les erreurs de type axios
+        if (err.response?.status === 401) {
+          setErrorMessage('Le mot de passe ne correspond pas');
+        }
+      } else {
+        // pour gérer les erreurs non axios
+        if (err instanceof Error) setErrorMessage(errorMessage);
+      }
+    }
+  };
 
   // ------------------ RETURN --------------------------------
   return (
     <div className="connectModal">
       <p className="connectModal__title">INSCRIPTION</p>
 
-      {/* {checkPassword && (
-        <p className="connectModal__title__error">le mot de passe ne correspont pas</p>
-      )} */}
+      <p>{errorMessage}</p>
 
-      <form>
+      <form onSubmit={postUser}>
+        {/* ----- FIRST NAME ----- */}
+        <div className="connectModal__">
+          <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
+            <TextField
+              id="outlined-basic"
+              value={firstName}
+              onChange={handleFirstName}
+              label="Prénom"
+              type="text"
+              autoComplete="current-text"
+              variant="outlined"
+              size="small"
+              required
+            />
+          </FormControl>
+        </div>
+
+        {/* ----- LAST NAME ----- */}
+        <div className="connectModal__">
+          <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
+            <TextField
+              id="outlined-basic"
+              value={lastName}
+              onChange={handleLastName}
+              label="Nom"
+              type="text"
+              autoComplete="current-text"
+              variant="outlined"
+              size="small"
+              required
+            />
+          </FormControl>
+        </div>
+
+        {/* ----- PHONE NUMBER ----- */}
+        <div className="connectModal__">
+          <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
+            <TextField
+              id="outlined-basic"
+              value={phoneNumber}
+              onChange={handlePhoneNumber}
+              label="Téléphone"
+              type="tel"
+              autoComplete="current-password"
+              variant="outlined"
+              size="small"
+              required
+            />
+          </FormControl>
+        </div>
+
         {/* ----- EMAIL INPUT ----- */}
-        <div className="connectModal__email">
+        <div className="connectModal__">
           <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
             <TextField
               id="outlined-basic"
               value={email}
               onChange={handleEmail}
               label="Email"
-              type="text"
+              type="email"
               autoComplete="current-password"
               variant="outlined"
               size="small"
@@ -141,7 +275,6 @@ const ConnectModal = () => {
             />
           </FormControl>
         </div>
-
         {/* ----- PASSWORD CONFIRMATION INPUT ----- */}
         <div className="connectModal__password__confirmation">
           <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
@@ -158,7 +291,6 @@ const ConnectModal = () => {
             />
           </FormControl>
         </div>
-
         {/* ----- ADDRESSLin1 INPUT ----- */}
         <div className="connectModal__address">
           <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
@@ -175,7 +307,6 @@ const ConnectModal = () => {
             />
           </FormControl>
         </div>
-
         {/* ----- ADDRESSLine2 INPUT ----- */}
         <div className="connectModal__address">
           <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
@@ -191,7 +322,6 @@ const ConnectModal = () => {
             />
           </FormControl>
         </div>
-
         {/* ----- ZIPCODE AND COUNTRY INPUT ----- */}
         <>
           <FormControl sx={{ m: 1, width: '50ch' }} variant="standard">
@@ -223,10 +353,8 @@ const ConnectModal = () => {
             </div>
           </FormControl>
         </>
-
         {/* ----- ERROR MESSAGE (IF PASSWORD IS WRONG)----- */}
         {/* {error && <p>{error}</p>} */}
-
         {/* ----- CHECKBOX & LOGIN BUTTON ----- */}
         <div className="connectModal__choices">
           <FormControlLabel
@@ -249,6 +377,8 @@ const ConnectModal = () => {
           />
           <div>
             {email !== '' &&
+            firstName !== '' &&
+            lastName !== '' &&
             password === confirmedPassword &&
             email.match(emailPattern) &&
             password !== '' &&
@@ -287,7 +417,7 @@ const ConnectModal = () => {
       </div>
 
       {/* ----- SOCIAL MEDIA CONNECTIONS----- */}
-      <div className="connectModal__socials">
+      {/* <div className="connectModal__socials">
         <GoogleLoginButton
           onClick={displayHello}
           text={googleText}
@@ -297,18 +427,18 @@ const ConnectModal = () => {
           onClick={displayHello}
           text={facebookText}
           align={socialIconAlign}
-        />
+        /> */}
 
-        {/* ----- LOG IN ----- */}
-        <div className="connectModal__logged">
-          <p>
-            Vous avez déjà un compte ?
-            <Link to="/seconnecter">
-              <span>Se connecter</span>
-            </Link>
-          </p>
-        </div>
+      {/* ----- LOG IN ----- */}
+      <div className="connectModal__logged">
+        <p>
+          Vous avez déjà un compte ?
+          <Link to="/seconnecter">
+            <span>Se connecter</span>
+          </Link>
+        </p>
       </div>
+      {/* </div> */}
     </div>
   );
 };
