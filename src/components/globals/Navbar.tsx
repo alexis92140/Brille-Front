e1d6a25ca387d19c9e05e66063b87f204bc1478d
unknown <mrbreenalexis@gmail.com>
2022-07-13 16:46:35 +0200

    CART ICON NOW INCREMENT THE ADDITIONALS ORDERS

--- src/components/globals/Navbar.tsx
+++ src/components/globals/Navbar.tsx
@@ -1,16 +1,19 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
 // ----- Package components -----
 import 'react-js-stickynav/dist/index.css';
+
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Badge, { BadgeProps } from '@mui/material/Badge';
+import IconButton from '@mui/material/IconButton';
+import { styled } from '@mui/material/styles';
 import { Sling as Hamburger } from 'hamburger-react';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { StickyNav } from 'react-js-stickynav';
 import { Link, NavLink } from 'react-router-dom';
+
 import navlinks from '../../../data/navLinks';
+import ShoppingCartContext from '../../Context/ShoppingCartContext';
 import Dropdown from './Dropdown';
-import Badge, { BadgeProps } from '@mui/material/Badge';
-import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 // ---------------Cart icon style config ----------------------------
 
@@ -43,6 +46,8 @@ const Navbar = () => {
   };
 
   window.addEventListener('scroll', changeColor);
+
+  const { cartItems } = useContext(ShoppingCartContext);
 
   return (
     <div>
@@ -87,7 +92,7 @@ const Navbar = () => {
               ))}
               <Link to="/panier" onClick={closeMenu}>
                 <IconButton aria-label="cart">
-                  <StyledBadge badgeContent={1} color="secondary">
+                  <StyledBadge badgeContent={cartItems.length} color="secondary">
                     <ShoppingCartIcon />
                   </StyledBadge>
                 </IconButton>
