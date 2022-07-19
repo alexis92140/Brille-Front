5c7aaee4b74004a1da922770098e49f37f758ee7
JacquesPoulin <jacques.poulin64@gmail.com>
2022-07-19 02:14:31 +0200

    [FEATURES] : New CardItem Component

--- src/components/cart/CartItem.tsx
+++ src/components/cart/CartItem.tsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import Paper from '@mui/material/Paper';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import ShoppingCartContext from '../../Context/ShoppingCartContext';
+import IProduct from '../../interfaces/IProduct';
+import axios from 'axios';
 
 const CartItem = () => {
+  // >> USE PARAMS
+  const { id } = useParams();
+
+  // >> STATES
+
+  const [selectedItem, setSelectedItem] = useState<IProduct>();
+
+  // >> ALL THE "SHOPPINGCARTCONTEXT" FUNCTIONS
+
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+    cartItems,
+  } = useContext(ShoppingCartContext);
+
+  // >> AXIOS
+
+  useEffect(() => {
+    const getAddedItem = async () => {
+      let url: string = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+
+      const { data } = await axios.get<IProduct>(url, {
+        withCredentials: true,
+      });
+      setSelectedItem(data);
+    };
+    getAddedItem();
+  }, [id]);
+
+  // >> VARIABLES
+  // from the shoppingcartcontext to get the items
+  const quantity = cartItems.length;
+
+  const shippingPrice = 3;
+
   return (
-    <div>
-      <h1>Cart Item</h1>
+    <div className="cartItem">
+      <Paper elevation={3}>
+        <div className="cartItem__productInfos">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/brille-handbags.appspot.com/o/bluebag.png?alt=media&token=fffcb6a1-3d32-49c4-90c2-1d11b5dedbff"
+            alt=""
+          />
+          <div>
+            <h4>CANCUN</h4>
+            <p>Sac en pépin de pomme</p>
+            <p>200 €</p>
+            <p>Stock: 2</p>
+          </div>
+          <div className="cartItem__productInfos__icons">
+            <Fab size="small" aria-label="add">
+              <RemoveIcon />
+            </Fab>
+            {quantity ? quantity : 0}
+            <Fab size="small" aria-label="add">
+              <AddIcon />
+            </Fab>
+          </div>
+        </div>
+      </Paper>
+      <hr />
+      <div>
+        <p>Sous-total</p>
+        <p>200 €</p>
+      </div>
+      <div>
+        <p>Frais de Livraison</p>
+        <p>{shippingPrice} €</p>
+      </div>
     </div>
   );
 };
--- src/components/cart/RightSide.tsx
+++ src/components/cart/RightSide.tsx
@@ -1,15 +1,6 @@
-import DeleteIcon from '@mui/icons-material/Delete';
-import PaymentsIcon from '@mui/icons-material/Payments';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import FormControl from '@mui/material/FormControl';
-import IconButton from '@mui/material/IconButton';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
-
+import { Link } from 'react-router-dom';
 import ShoppingCartContext from '../../Context/ShoppingCartContext';
 import IProduct from '../../interfaces/IProduct';
 import GoToTop from '../globals/GoToTop';
@@ -19,41 +10,10 @@ import CartItem from './CartItem';
 
 const RightSide = () => {
   // >> USE PARAMS
-  const { id } = useParams();
-
-  // >> STATES
-  const [cartProduct, setCartProduct] = useState<IProduct>();
-  const [quantity, setQuantity] = useState<string>('1');
-
-  // >> FUNCTIONS
-  const handleChange = (e: SelectChangeEvent) => {
-    setQuantity(e.target.value || '1');
-  };
-
-  // >> VARIABLES
-  const { cartItems } = useContext(ShoppingCartContext);
-  console.log(cartItems);
-
-  // >> AXIOS
-
-  useEffect(() => {
-    const getCartItem = async () => {
-      let url: string = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
-
-      const { data } = await axios.get<IProduct>(url, {
-        withCredentials: true,
-      });
-      setCartProduct(data);
-      console.log(data);
-    };
-    getCartItem();
-    console.log(cartProduct);
-  }, [id]);
-
   return (
     <div className="right">
       {/* Right part of the Cart component */}
-
+      <h1>Mon Panier</h1>
       <div className="right__wrapper">
         {/* First part of the Cart component */}
         <CartItem />
