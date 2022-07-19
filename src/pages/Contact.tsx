50dc90b1eb30ef243c5dbf47c315dbb7ff1d8582
unknown <mrbreenalexis@gmail.com>
2022-07-19 14:29:48 +0200

    Responsive contact page and few last minutes customer request

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,17 +3,19 @@ import 'react-toastify/dist/ReactToastify.css';
 import SendIcon from '@mui/icons-material/Send';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
+import { grey } from '@mui/material/colors';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import MediaQuery from 'react-responsive';
 import { toast, ToastContainer } from 'react-toastify';
 
+import contactResponsive from '../components/contact/contactResponsive';
 import GoToTop from '../components/globals/GoToTop';
 import IImage from '../interfaces/IImage';
 import IPage from '../interfaces/IPage';
-
-const Contact = () => {
+const Contact = (props) => {
   const notify = () => toast(' Votre message à bien été envoyé !!  ');
 
   // Ici j'appel mon interface image >>
@@ -51,80 +53,142 @@ const Contact = () => {
     getOnePage();
   }, []);
 
-  return (
-    <div className="contact">
-      <div className="contact__head">
-        <img id="plage" src={allImages?.image} alt="plage bleu" />
-      </div>
+  const color = grey[900];
 
-      <div className="contact__container">
-        <div className="contact__container__titleWrapper">
-          <h1>Nous contacter</h1>
+  return (
+    <>
+      <MediaQuery query="(min-width: 1000px)">
+        <div className="contact">
+          <div className="contact__head">
+            <img id="plage" src={allImages?.image} alt="plage bleu" />
+          </div>
+
+          <div className="contact__container">
+            <div className="contact__container__titleWrapper">
+              <h1>Nous contacter</h1>
+            </div>
+            <form
+              action="https://getform.io/f/e3635432-5c41-4623-a26f-1e7063e12bc9"
+              method="POST"
+              target="_blank"
+              className="contact__container__form">
+              <div className="contact__container__form__credentials">
+                <TextField
+                  id="standard-basic"
+                  type="text"
+                  name="Nom"
+                  label="Nom"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+                <TextField
+                  id="standard-basic"
+                  type="text"
+                  label="Prénom"
+                  name="Prénom"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+              </div>
+              <div className="contact__container__form__mail">
+                <TextField
+                  id="standard-basic"
+                  type="email"
+                  label="Email"
+                  name="Email"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+              </div>
+              <div className="contact__container__form__textArea">
+                <TextField
+                  id="outlined-multiline-static"
+                  type="text"
+                  label="Votre demande"
+                  name="votre demande"
+                  placeholder="Ecrivez votre texte ici"
+                  multiline
+                  rows={4}
+                  sx={{ m: 1, width: '100%' }}
+                />
+              </div>
+              <div className="contact__container__form__button">
+                <Button
+                  sx={{ color: { color } }}
+                  style={{ backgroundColor: '#f4f7f5' }}
+                  onClick={notify}
+                  type="submit"
+                  variant="contained"
+                  endIcon={<SendIcon />}>
+                  Envoyer
+                </Button>
+                <ToastContainer />
+              </div>
+            </form>
+          </div>
+          <GoToTop />
         </div>
-        <form
-          action="https://getform.io/f/e3635432-5c41-4623-a26f-1e7063e12bc9"
-          method="POST"
-          target="_blank"
-          className="contact__container__form">
-          <div className="contact__container__form__credentials">
-            <TextField
-              id="standard-basic"
-              type="text"
-              name="Nom"
-              label="Nom"
-              variant="standard"
-              sx={{ m: 1, width: '40ch' }}
-            />
-            <TextField
-              id="standard-basic"
-              type="text"
-              label="Prénom"
-              name="Prénom"
-              variant="standard"
-              sx={{ m: 1, width: '40ch' }}
-            />
+      </MediaQuery>
+      <div>
+        <MediaQuery query="(max-width: 1000px)">
+          <div className="mainContainer">
+            <h1>Nous contacter</h1>
+            <div className="mainContainer__formContainer">
+              <form
+                action="https://getform.io/f/e3635432-5c41-4623-a26f-1e7063e12bc9"
+                method="POST"
+                target="_blank">
+                <TextField
+                  id="standard-basic"
+                  type="text"
+                  name="Nom"
+                  label="Nom"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+                <TextField
+                  id="standard-basic"
+                  type="text"
+                  label="Prénom"
+                  name="Prénom"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+                <TextField
+                  id="standard-basic"
+                  type="email"
+                  label="Email"
+                  name="Email"
+                  variant="standard"
+                  sx={{ m: 1, width: '40ch' }}
+                />
+                <TextField
+                  id="outlined-multiline-static"
+                  type="text"
+                  label="Votre demande"
+                  name="votre demande"
+                  placeholder="Ecrivez votre texte ici"
+                  multiline
+                  rows={4}
+                  sx={{ m: 1, width: '100%' }}
+                />
+                <Button
+                  sx={{ color: { color } }}
+                  style={{ backgroundColor: '#f4f7f5' }}
+                  onClick={notify}
+                  type="submit"
+                  variant="contained"
+                  className="ainContainer__formContainer__buttonSent"
+                  endIcon={<SendIcon />}>
+                  Envoyer
+                </Button>
+                <ToastContainer />
+              </form>
+            </div>
           </div>
-          <div className="contact__container__form__mail">
-            <TextField
-              id="standard-basic"
-              type="email"
-              label="Email"
-              name="Email"
-              variant="standard"
-              sx={{ m: 1, width: '40ch' }}
-            />
-          </div>
-          <div className="contact__container__form__textArea">
-            <TextField
-              id="outlined-multiline-static"
-              type="text"
-              label="Votre demande"
-              name="votre demande"
-              placeholder="Ecrivez votre texte ici"
-              multiline
-              rows={4}
-              sx={{ m: 1, width: '100%' }}
-            />
-          </div>
-          <div className="contact__container__form__button">
-            <FormControlLabel
-              control={<Checkbox />}
-              label="Je ne suis pas un robot"
-              sx={{ '& .MuiSvgIcon-root': { fontSize: 32 } }}
-            />
-            <Button
-              onClick={notify}
-              type="submit"
-              variant="contained"
-              endIcon={<SendIcon />}>
-              Envoyer
-            </Button>
-            <ToastContainer />
-          </div>
-        </form>
+        </MediaQuery>
       </div>
-      <GoToTop />
-    </div>
+    </>
   );
 };
 
